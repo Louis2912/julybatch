cc556df7ace4f80fb77ed7e9b2d3296d2bf52331
Louis2912 <Louis.Ebi@paragondigitalservices.com>
2023-07-14 12:22:56 +0530

    14.07 Final commit

--- src/hashmap.java
+++ src/hashmap.java
@@ -1,2 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class hashmap {
+    public static void main(String[] args) {
+
+        HashMap<String,Object> emplist = new HashMap<String,Object>();
+
+        emplist.put("Empname","Sam");
+        emplist.put("Emp ID","AB12");
+        emplist.put("Salary",10000);
+        emplist.put("Dept","IT");
+        emplist.put("DOJ","14.07.2023");
+
+        System.out.println("Emp list is "+emplist);
+
+        for (Map.Entry keylist : emplist.entrySet()){
+            System.out.println(keylist.getValue());
+        }
+
+
+    }
+
 }
